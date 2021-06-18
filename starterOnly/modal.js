7530abe3f600761e381534d753898150de34f0ed
Christophe  ADOTEVI <81805739+Christado@users.noreply.github.com>
2021-06-18 12:36:11 +0200

    première modif Html

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+let form = document.querySelector('#loginForm');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,25 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// Surveiller  la modification du prénom
+form.Prénom.addEventListener('change' , function() {
+   validPrénom(this);
+});
 
+const validPrénom = function(inputPrénom) {
+  let PrénomRegExp = new RegExp(
+     '^[a-zA-Z]+','g'
+  );
+  let testPrénom = PrénomRegExp.test(inputPrénom.value);
+  let small = inputPrénom.nextElementSibling;
+  if(testPrénom) {
+    small.innerHTML = 'Prénom valide';
+    validPrénom.add('text-success');
+  } else {
+    small.innerHTML = 'Prénom Non Valide';
+    small.classList.add('text-danger');
+  }
+
+
+};
 
